bea3605c0af4483df3687ae60f49d3ade7efdcb0
sudharshanpkpm <sudharshanp256@gmail.com>
2023-05-03 19:47:24 +0530

    Create reqres.js

---
+++ reqres.js
@@ -0,0 +1,10 @@
+var http = require("http");
+function samp(request,response)
+{
+    response.writeHead(200,{"content-Type":"text/html"});
+    response.write("Hello");
+    response.end("<html><body>Url was:"+request.url+"</body></html>");
+    console.log("Request received");
+}
+http.createServer(samp).listen(8000);
+console.log("Server running");
